425ab8ad403e57f81295033f63cd33f3606c28fb
Federico Krenn <fedekrenn@gmail.com>
2022-10-03 18:38:46 -0300

    first commit

---
+++ ejs/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const routerProductos = require('./src/routes/productos')
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/productos', routerProductos);
+app.use(express.static('public'));
+
+
+const PORT = process.env.PORT || 8080;
+
+const server = app.listen(PORT, () => console.log(`Servidor http escuchando en el puerto ${server.address().port} | Modo EJS`));
+server.on('error', error => console.log(`Error en servidor ${error}`));
